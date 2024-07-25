1ba6a1499c017ec31f85753ba6b1555f26b1c851
Vlad <valdispatriarh@gmail.com>
2024-07-25 22:31:45 +0200

    test

--- Coffee-House/js/main.js
+++ Coffee-House/js/main.js
@@ -1,9 +1,8 @@
-
 // JavaScript для обработки клика по кнопке бургер-меню
-const burgerMenuButton = document.getElementById('burgerBtn')
-const mainNav = document.getElementById('mainNav')
+// const burgerMenuButton = document.getElementById("burgerBtn");
+// const mainNav = document.getElementById("mainNav");
 
-burgerMenuButton.addEventListener('click', () => {
-    // Переключаем класс для изменения положения меню
-    mainNav.classList.toggle('show-menu')
-})
+// burgerMenuButton.addEventListener("click", () => {
+//   // Переключаем класс для изменения положения меню
+//   mainNav.classList.toggle("show-menu");
+// });
